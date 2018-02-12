b7d03f8800ebf2d6afaf1504cbeabd3c7cc841c7
HangYongmao <1981217256@qq.com>
2018-02-12 22:05:00 +0800

    支持手势滑动

--- app/src/main/java/com/example/viewflipperdemo/MainActivity.java
+++ app/src/main/java/com/example/viewflipperdemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.viewflipperdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.widget.ImageView;
 import android.widget.ViewFlipper;
 
@@ -9,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ViewFlipper flipper;
     private int[] resId = {R.drawable.pic1, R.drawable.pic2, R.drawable.pic3, R.drawable.pic4};
+    private float startX;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +23,46 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < resId.length; i++) {
             flipper.addView(getImageView(resId[i]));
         }
-        // 为ViewFlipper添加进入时的动画效果
-        flipper.setInAnimation(this, R.anim.left_in);
-        // 为ViewFlipper添加退出时的动画效果
-        flipper.setOutAnimation(this, R.anim.left_out);
-        // 设置ViewFlipper的时间间隔
-        flipper.setFlipInterval(3000);
-        flipper.startFlipping();
+//        // 为ViewFlipper添加进入时的动画效果
+//        flipper.setInAnimation(this, R.anim.left_in);
+//        // 为ViewFlipper添加退出时的动画效果
+//        flipper.setOutAnimation(this, R.anim.left_out);
+//        // 设置ViewFlipper的时间间隔
+//        flipper.setFlipInterval(3000);
+//        flipper.startFlipping();
+    }
+
+    // 手势
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            // 手指落下
+            case MotionEvent.ACTION_DOWN:
+                startX = event.getX();
+                break;
+
+            // 手指滑动
+            case MotionEvent.ACTION_MOVE:
+                if (event.getX() - startX > 100) {
+                    // 向右滑动 看前一页
+                    flipper.setInAnimation(this, R.anim.left_in);
+                    flipper.setOutAnimation(this, R.anim.left_out);
+                    flipper.showPrevious();// 显示前一页
+                } else if (startX - event.getX() > 100) {
+                    // 向左滑动 看后一页
+                    flipper.setInAnimation(this, R.anim.right_in);
+                    flipper.setOutAnimation(this, R.anim.right_out);
+                    flipper.showNext();// 显示后一页
+                }
+                break;
+
+            // 手指离开
+            case MotionEvent.ACTION_UP:
+                break;
+            default:
+                break;
+        }
+        return super.onTouchEvent(event);
     }
 
     private ImageView getImageView(int resId) {
